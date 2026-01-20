4e2c9a56147bef2604783512a2c085d73f532488
dmgeoghegan <dmgeoghegan@gmail.com>
2026-01-20 15:13:24 -0500

    providers.tf: fixed dependencies, module.eks --> aws_eks_cluster and aws_eks_cluster_auth data

--- providers.tf
+++ providers.tf
@@ -5,11 +5,13 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
